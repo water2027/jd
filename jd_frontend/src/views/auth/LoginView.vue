9659d5b27cc0cb357c3e84b93068a61e4c043ad2
water <linshy76@mail2.sysu.edu.cn>
2025-05-06 23:30:45 +0800

    unocss && 登录/注册/重置密码页面

---
+++ jd_frontend/src/views/auth/LoginView.vue
@@ -0,0 +1,73 @@
+<script lang="ts" setup>
+import { ref, useTemplateRef } from 'vue'
+
+import FormContainer from '@/components/FormContainer.vue'
+import type { CustomFormData } from '@/composables/FormExam'
+import { useFormExam } from '@/composables/FormExam'
+import { showMsg } from '@/components/MessageBox.tsx'
+
+// import { login } from '@/api/user/login'
+import apiBus from '@/utils/apiBus'
+
+const rememberMe = useTemplateRef('rememberMe')
+
+const loginForm = ref<CustomFormData[]>([
+  {
+    id: 'email',
+    value: '',
+    label: '邮箱',
+    type: 'email',
+    autocomplete: 'email',
+  },
+  {
+    id: 'password',
+    value: '',
+    label: '密码',
+    type: 'password',
+    autocomplete: 'current-password',
+  },
+])
+
+const correct = useFormExam(loginForm)
+
+const loginAction = async () => {
+  const email = loginForm.value[0].value
+  const password = loginForm.value[1].value
+
+  try {
+    const resp = await login({ email, password })
+  
+    apiBus.emit('API:LOGIN', resp)
+  
+    if(rememberMe.value?.checked) {
+      localStorage.setItem('email', email)
+      localStorage.setItem('password', password)
+    }
+  } catch (error) {
+    console.error('Login failed:', error)
+    showMsg(error as string)
+    return 
+  }
+}
+</script>
+
+<template>
+  <FormContainer
+    class="w-full sm:w-1/2 mt-5"
+    @submit-form="loginAction"
+    :form-data="loginForm"
+    :disabled="!correct"
+  >
+    <div class="flex flex-row justify-between">
+      <div class="mx-a">
+        <input type="checkbox" ref="rememberMe">
+          <label for="rememberMe">记住我</label>
+          <span class="text-sm text-gray-500">下次自动登录</span>
+        </input>
+      </div>
+      <RouterLink class="mx-a text-purple-800" to="/auth/register">
+        注册个账号先
+      </RouterLink>
+    </div>
+  </FormContainer>
+</template>
---
+++ jd_frontend/src/views/auth/RegisterView.vue
@@ -0,0 +1,138 @@
+<script lang="ts" setup>
+import { computed, ref, useTemplateRef } from 'vue'
+
+import FormContainer from '@/components/FormContainer.vue'
+import type { CustomFormData } from '@/composables/FormExam'
+import { useFormExam } from '@/composables/FormExam'
+import { showMsg } from '@/components/MessageBox.tsx'
+
+import { register } from '@/api/user/register'
+import { sendCode } from '@/api/user/send_code'
+
+import apiBus from '@/utils/apiBus'
+
+const rememberMe = useTemplateRef('rememberMe')
+
+const passwordReg = /^\S*(?=\S{6,})(?=\S*\d)(?=\S*[A-Z])(?=\S*[a-z])(?=\S*[!@#$%^&*? ])\S*$/
+
+const registerForm = ref<CustomFormData[]>([
+  {
+    id: 'username',
+    value: '',
+    label: '用户名',
+    autocomplete: 'username',
+  },
+  {
+    id: 'email',
+    value: '',
+    label: '邮箱',
+    type: 'email',
+    autocomplete: 'email',
+  },
+  {
+    id: 'password',
+    value: '',
+    label: '密码',
+    type: 'password',
+    reg: passwordReg,
+    autocomplete: 'new-password',
+  },
+  {
+    id: 'password2',
+    value: '',
+    label: '确认密码',
+    reg:passwordReg,
+    type: 'password',
+  },
+  {
+    id: 'v_code',
+    value: '',
+    label: '验证码',
+  },
+])
+
+const emailIsCorrect = computed(() => {
+  const email = registerForm.value[1].value
+  const reg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  return reg.test(email)
+})
+
+const correct = useFormExam(registerForm)
+
+const passwordIsCorrect = computed(() => {
+  const password = registerForm.value[2].value
+  const password2 = registerForm.value[3].value
+  return password === password2 && passwordReg.test(password)
+})
+
+const sendCodeAction = async () => {
+  const email = registerForm.value[1].value
+  if (!emailIsCorrect.value) {
+    return
+  }
+  await sendCode({ email })
+}
+
+const registerAction = async () => {
+  if (!correct.value) {
+    return
+  }
+
+  const name = registerForm.value[0].value
+  const email = registerForm.value[1].value
+  const password = registerForm.value[2].value
+  const password2 = registerForm.value[3].value
+  const vcode = registerForm.value[4].value
+
+  try {    
+    const resp = await register({ name, email, password, password2, vcode })
+  
+    if(rememberMe.value?.checked) {
+      localStorage.setItem('email', email)
+      localStorage.setItem('password', password)
+    }
+    console.log('register')
+    apiBus.emit('API:LOGIN',resp)
+  } catch (error) {
+    console.error('Register failed:', error)
+    showMsg(error as string)
+    return
+  }
+}
+</script>
+
+<template>
+  <FormContainer
+    class="w-full sm:w-1/2 mt-5"
+    @submit-form="registerAction"
+    :form-data="registerForm"
+    :disabled="!correct"
+  >
+  <span v-if="!passwordIsCorrect" class="text-red-500 text-sm">
+    密码至少6位，包含大小写字母、数字和!@#$%^&*? 中的一个
+    <br>
+  </span>
+  <span v-else class="text-green-500 text-sm block mx-a">
+    密码符合要求
+  </span>
+  <div class="flex flex-row justify-between">
+      <div class="mx-a">
+        <input type="checkbox" ref="rememberMe">
+          <label for="rememberMe">记住我</label>
+          <span class="text-sm text-gray-500">下次自动登录</span>
+        </input>
+      </div>
+      <RouterLink class="mx-a text-purple-800" to="/auth/login">
+        有账号了
+      </RouterLink>
+    </div>
+    <button
+      type="button"
+      @click.prevent="sendCodeAction"
+      :disabled="!emailIsCorrect"
+      class="w-full h-10 bg-[#eb6b26] text-white border-0 text-lg cursor-pointer mt-5 rounded-[20px] flex justify-center items-center hover:bg-[#ff7e3b] disabled:bg-zinc-600"
+    >
+      {{ emailIsCorrect ? '发送验证码' : '请填写正确邮箱' }}
+    </button>
+  </FormContainer>
+</template>
